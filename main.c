ec222a1a97060c1090a8164512df48c956de4289
seanerchen <seanerchen@gmail.com>
2014-05-06 15:57:46 +0800

    add colors

--- main.c
+++ main.c
@@ -1,3 +1,6 @@
+#include <math.h> 
+#include <stdlib.h> 
+#include <string.h> 
 #include "2048.h"
 #include "win.h"
 
@@ -14,45 +17,38 @@ int main(void)
     initArr(arr);
 
     initscr();/* Start curses mode */
-    //start_color();/* Start the color functionality */
     cbreak();/* Line buffering disabled, Pass on * everty thing to me */
     keypad(stdscr, TRUE);/* I need that nift                        y F1 */
     noecho();
-    //init_pair(1, COLOR_CYAN, COLOR_BLACK);
+    start_color();/* Start the color functionality */
 
     printWin(arr);
 
     /* Initialize the window parameters */
     mvprintw(0,0,"%s","Press F1 to exit");
-
     while((ch = getch()) != KEY_F(1))
     //while((ch = getchar())){
     {
-        clear();
         switch(ch)
         {
             case KEY_LEFT:
             case 'h':
             leftArrow(arr);
-            //echoArr(arr);
             printWin(arr);
             break;
             case KEY_RIGHT:
             case 'l':
             rightArrow(arr);
-            //echoArr(arr);
             printWin(arr);
             break;
             case KEY_UP:
             case 'k':
             upArrow(arr);
-            //echoArr(arr);
             printWin(arr);
             break;
             case KEY_DOWN:
             case 'j':
             downArrow(arr);
-            //echoArr(arr);
             printWin(arr);
             break;
         }
@@ -64,6 +60,16 @@ int main(void)
 
 void printWin(int arr[HIGHT][WIDTH])
 {
+    init_pair(1, COLOR_RED, COLOR_BLACK);
+    init_pair(2, COLOR_GREEN, COLOR_BLACK);
+    init_pair(3, COLOR_YELLOW, COLOR_BLACK);
+    init_pair(4, COLOR_BLUE, COLOR_BLACK);
+    init_pair(5, COLOR_MAGENTA, COLOR_BLACK);
+    init_pair(6, COLOR_CYAN, COLOR_BLACK);
+    init_pair(7, COLOR_WHITE, COLOR_BLACK);
+
+    init_pair(8, COLOR_WHITE, COLOR_YELLOW);
+
     WIN win[HIGHT][WIDTH];
     int i, j;
     for(i = 0; i < HIGHT; i++)
@@ -71,11 +77,17 @@ void printWin(int arr[HIGHT][WIDTH])
         for(j = 0; j < WIDTH; j++)
         {
             WIN *thewin = &win[i][j];
-            init_win_params(&win[i][j], i, j);
+            init_win_params(&win[i][j], j-3, i-3);
             print_win_params(&win[i][j]);
+            attron(COLOR_PAIR(7));
             create_box(&win[i][j], TRUE);
-            mvprintw(thewin->starty + WINY/2 , thewin->startx + WINX/2,"%d", arr[i][j]);
+            attroff(COLOR_PAIR(7));
+            mvprintw(thewin->starty + WINY/2 , thewin->startx + WINX/2-4,"%s", "         ");
+            attron(COLOR_PAIR((int)log2(arr[i][j])%7 + 1));
+            mvprintw(thewin->starty + WINY/2 , thewin->startx + WINX/2-(int)(log10(arr[i][j]) + 1/2),"%d", arr[i][j]);
+            attroff(COLOR_PAIR((int)log2(arr[i][j])%7 + 1));
         }
     }
     refresh();
 }
+
